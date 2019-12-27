d74e2f85b1c9a32320d5ecb4c1f061ea7befe7ae
chinabjy <48501377+chinabjy@users.noreply.github.com>
2019-12-27 17:17:20 +0800

    Update hq_server.sh

--- hq_server.sh
+++ hq_server.sh
@@ -15,3 +15,53 @@ cmd1=$"sleep 10s; php -f /var/www/html/v1/get_hq/get_hq_eosbtc_huobi.php";
 screen -x -S $screen_name1 -p 0 -X stuff "$cmd1"
 screen -x -S $screen_name1 -p 0 -X stuff $'\n'
 screen -x -S $screen_name1 -p 0 -X stuff "exit"
+#第二个
+screen_name2=$"EOS_hq_kucoin"
+screen -dmS $screen_name1
+cmd2=$"sleep 10s; php -f /var/www/html/v1/get_hq/get_hq_eosbtc_kucoin.php";
+screen -x -S $screen_name1 -p 0 -X stuff "$cmd2"
+screen -x -S $screen_name1 -p 0 -X stuff $'\n'
+screen -x -S $screen_name1 -p 0 -X stuff "exit"
+#
+screen_name3=$"ETN_hq_huobi"
+screen -dmS $screen_name1
+cmd3=$"sleep 10s; php -f /var/www/html/v1/get_hq/get_hq_etnbtc_huobi.php";
+screen -x -S $screen_name1 -p 0 -X stuff "$cmd3"
+screen -x -S $screen_name1 -p 0 -X stuff $'\n'
+screen -x -S $screen_name1 -p 0 -X stuff "exit"
+#
+screen_name4=$"ETN_hq_kucoin"
+screen -dmS $screen_name1
+cmd4=$"sleep 10s; php -f /var/www/html/v1/get_hq/get_hq_etnbtc_kucoin.php";
+screen -x -S $screen_name1 -p 0 -X stuff "$cmd4"
+screen -x -S $screen_name1 -p 0 -X stuff $'\n'
+screen -x -S $screen_name1 -p 0 -X stuff "exit"
+#
+screen_name5=$"UTK_hq_huobi"
+screen -dmS $screen_name1
+cmd5=$"sleep 10s; php -f /var/www/html/v1/get_hq/get_hq_utkbtc_huobi.php";
+screen -x -S $screen_name1 -p 0 -X stuff "$cmd5"
+screen -x -S $screen_name1 -p 0 -X stuff $'\n'
+screen -x -S $screen_name1 -p 0 -X stuff "exit"
+#
+screen_name6=$"UTK_hq_kucoin"
+screen -dmS $screen_name1
+cmd6=$"sleep 10s; php -f /var/www/html/v1/get_hq/get_hq_utkbtc_kucoin.php";
+screen -x -S $screen_name1 -p 0 -X stuff "$cmd6"
+screen -x -S $screen_name1 -p 0 -X stuff $'\n'
+screen -x -S $screen_name1 -p 0 -X stuff "exit"
+#
+screen_name7=$"DBC_hq_huobi"
+screen -dmS $screen_name1
+cmd7=$"sleep 10s; php -f /var/www/html/v1/get_hq/get_hq_dbcbtc_huobi.php";
+screen -x -S $screen_name1 -p 0 -X stuff "$cmd7"
+screen -x -S $screen_name1 -p 0 -X stuff $'\n'
+screen -x -S $screen_name1 -p 0 -X stuff "exit"
+#
+screen_name8=$"DBC_hq_kucoin"
+screen -dmS $screen_name1
+cmd8=$"sleep 10s; php -f /var/www/html/v1/get_hq/get_hq_dbcbtc_kucoin.php";
+screen -x -S $screen_name1 -p 0 -X stuff "$cmd8"
+screen -x -S $screen_name1 -p 0 -X stuff $'\n'
+screen -x -S $screen_name1 -p 0 -X stuff "exit"
+#
