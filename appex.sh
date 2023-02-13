f858aa2383509c6040b279a13407248dc6b24e1c
chinabjy <48501377+chinabjy@users.noreply.github.com>
2023-02-13 17:31:01 +0800

    Update appex.sh

--- appex.sh
+++ appex.sh
@@ -113,8 +113,11 @@ mkdir -p /appex/etc
 mkdir -p /appex/bin
 MAC=$(ifconfig "$Eth" |awk '/HWaddr/{ print $5 }')
 [ -z "$MAC" ] && MAC=$(ifconfig "$Eth" |awk '/ether/{ print $2 }')
+echo -ne 'serverSpeeder has been removed!666 \n\n\n'
 [ -z "$MAC" ] && Uninstall && echo "Not Found MAC address! " && exit 1
+echo -ne 'serverSpeeder has been removed!777 \n\n\n'
 wget --no-check-certificate -q -O "/appex/etc/apx.lic" "https://moeclub.azurewebsites.net/lic?mac=$MAC"
+
 [ "$(du -b /appex/etc/apx.lic |awk '{ print $1 }')" -ne '152' ] && Uninstall && echo "Error! I can not generate the Lic for you, Please try again later! " && exit 1
 echo "Lic generate success! "
 [ -n $(which ethtool) ] && rm -rf /appex/bin/ethtool && cp -f $(which ethtool) /appex/bin
