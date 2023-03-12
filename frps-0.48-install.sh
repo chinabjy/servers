91886b14eb1831a38bf6a181664b43d817cfdf95
chinabjy <48501377+chinabjy@users.noreply.github.com>
2023-03-12 18:35:28 +0800

    Create frps-0.48-install.sh

---
+++ frps-0.48-install.sh
@@ -0,0 +1,9 @@
+systemctl stop firewalld.service 
+systemctl disable firewalld.service
+wget https://github.com/fatedier/frp/releases/download/v0.48.0/frp_0.48.0_linux_amd64.tar.gz
+tar -zxvf frp_0.48.0_linux_amd64.tar.gz
+wget -qO /lib/systemd/system/frps.service https://raw.githubusercontent.com/chinabjy/servers/master/frps.service
+wget -qO /root/frp_0.33.0_linux_amd64/frps.ini https://raw.githubusercontent.com/chinabjy/servers/master/frps.ini
+systemctl start frps  
+systemctl enable frps
+systemctl status frps
