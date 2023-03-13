6242d03452058ebf8832663d6a274719786b52b3
chinabjy <48501377+chinabjy@users.noreply.github.com>
2023-03-13 14:30:16 +0800

    Update frps-0.48-install.sh

--- frps-0.48-install.sh
+++ frps-0.48-install.sh
@@ -5,6 +5,7 @@ wget https://github.com/fatedier/frp/releases/download/v0.48.0/frp_0.48.0_linux_
 tar -zxvf frp_0.48.0_linux_amd64.tar.gz
 wget -qO /lib/systemd/system/frps.service https://raw.githubusercontent.com/chinabjy/servers/master/frps048.service
 wget -qO /root/frp_0.48.0_linux_amd64/frps.ini https://raw.githubusercontent.com/chinabjy/servers/master/frps.ini
+systemctl daemon-reload
 systemctl start frps  
 systemctl enable frps
 systemctl status frps
