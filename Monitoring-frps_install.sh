df83b6960eb13ddf479e9a80b9728df6ff6473e5
chinabjy <48501377+chinabjy@users.noreply.github.com>
2023-03-29 19:26:43 +0800

    Update Monitoring-frps_install.sh

--- Monitoring-frps_install.sh
+++ Monitoring-frps_install.sh
@@ -1,3 +1,7 @@
+#1安装php
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm ;
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm ;
+yum -y install php72w php72w-cli php72w-common php72w-devel php72w-mysqlnd php72w-gd php72w-imap php72w-ldap php72w-odbc php72w-pear php72w-xml php72w-xmlrpc ;
 yum install screen -y
 wget -O /var/www/html/Monitoring-frps.php https://raw.githubusercontent.com/chinabjy/servers/master/Monitoring-frps.php
 wget -O /var/www/html/frps_autorun.sh https://raw.githubusercontent.com/chinabjy/servers/master/frps_autorun.sh
