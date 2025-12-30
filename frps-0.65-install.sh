69d5d5d71ba89dc02b11858e8356ad2e5f97e8b5
chinabjy <48501377+chinabjy@users.noreply.github.com>
2025-12-30 20:20:21 +0800

    Update frps-0.65-install.sh

--- frps-0.65-install.sh
+++ frps-0.65-install.sh
@@ -3,7 +3,7 @@ systemctl stop frps.service
 wget https://github.com/fatedier/frp/releases/download/v0.65.0/frp_0.65.0_linux_arm64.tar.gz
 tar -zxvf frp_0.65.0_linux_amd64.tar.gz
 wget -qO /lib/systemd/system/frps.service https://raw.githubusercontent.com/chinabjy/servers/master/frps065.service
-wget -qO /root/frp_0.65.0_linux_amd64/frps.ini https://raw.githubusercontent.com/chinabjy/servers/master/frps.ini
+wget -qO /root/frp_0.65.0_linux_amd64/frps.toml https://raw.githubusercontent.com/chinabjy/servers/master/frps.toml
 systemctl daemon-reload
 systemctl start frps  
 systemctl enable frps
